5f04d9929f0c1ec96e55ec4473de6b5a4237f8f0
Tri Vo <tri.minh.vo@mgm-tp.com>
2020-04-30 10:03:56 +0700

    init backend

---
+++ src/main/java/travel/be/BeApplication.java
@@ -0,0 +1,12 @@
+package travel.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeApplication.class, args);
+    }
+
+}
